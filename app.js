ce77ae3f917a8032ad1a1c22635ca04c014d5239
Linacolor <annafragile123@gmail.com>
2021-09-10 00:25:35 +0300

    Добавляет разметку элементов галереи и событие клика

--- app.js
+++ app.js
@@ -62,4 +62,38 @@ const galleryItems = [
       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
     description: 'Lighthouse Coast Sea',
   },
-];
+]
+const galleryEl = document.querySelector('.js-gallery')
+// console.log(galleryEl)
+const imageMarkup = createImageCardMarkup(galleryItems)
+
+galleryEl.insertAdjacentHTML('beforeend', imageMarkup)
+
+function createImageCardMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    })
+    .join('')
+}
+
+galleryEl.addEventListener('click', onImageClick)
+
+function onImageClick(event) {
+  // console.log(event.target.nodeName)
+  if (event.target.nodeName !== 'IMG') {
+    return
+  }
+}
