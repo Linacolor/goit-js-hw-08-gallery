324e9969d7ab8c57130a2eeda73cb2791027368b
Linacolor <annafragile123@gmail.com>
2021-09-12 00:58:59 +0300

    Закрытие модального окна

--- js/app.js
+++ js/app.js
@@ -26,15 +26,35 @@ function createImageCardMarkup(galleryItems) {
     .join('')
 }
 
-galleryEl.addEventListener('click', onImageClick)
+const modalEl = document.querySelector('.js-lightbox')
+// console.log(modalEl)
+const imgEl = document.querySelector('.lightbox__image')
+// console.log(imgEl)
+galleryEl.addEventListener('click', openModal)
 
-function onImageClick(event) {
+function openModal(event) {
   // console.log(event.target.nodeName)
-  if (event.target.nodeName !== 'IMG') {
-    return
+  event.preventDefault()
+  // console.log(event.target)
+  // console.log(event.target.dataset.source)
+  modalEl.classList.add('is-open')
+  imgEl.src = event.target.dataset.source
+}
+const buttonEl = document.querySelector('.lightbox__button')
+// console.log(buttonEl)
+buttonEl.addEventListener('click', closeModal)
+function closeModal(event) {
+  modalEl.classList.remove('is-open')
+  imgEl.src = ''
+}
+// Закрытие модального окна по клику на div.lightbox__overlay
+const divEl = document.querySelector('.lightbox__overlay')
+divEl.addEventListener('click', closeModal)
+
+// Закрытие модального окна по нажатию клавиши ESC
+window.addEventListener('keydown', closeModalOnEsc)
+function closeModalOnEsc(event) {
+  if (event.code === 'Escape') {
+    modalEl.classList.remove('is-open')
   }
 }
-// const modal = document.querySelector('js-lightbox')
-// // console.log(modal)
-// modal.addEventListener('click', closeModal)
-// function closeModal() {}
